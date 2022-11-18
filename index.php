c9b99eace2bb20d0f6f2c6e9672768abed965da3
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-11-18 12:00:53 +0100

    First setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'bootstrap.php';
+
+//Base Template
+// $templateParams["titolo"] = "Blog TW - Home";
+// $templateParams["nome"] = "lista-articoli.php";
+// $templateParams["categorie"] = $dbh->getCategories();
+// $templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
+// //Home Template
+// $templateParams["articoli"] = $dbh->getPosts(2);
+
+require 'template/base.php';
+?>
