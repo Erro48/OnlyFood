89b3078be1e90096efa633746841acb60d28d7a4
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-12-07 19:47:31 +0100

    Rename base style file

--- template/login-base.php
+++ template/login-base.php
@@ -11,7 +11,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/baseStyle.css">
+    <link rel="stylesheet" href="./style/base-style.css">
     <link rel="stylesheet" href="./style/login-style.css">
 </head>
 <body>
