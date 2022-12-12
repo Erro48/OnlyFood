2cc5cc853cdc1c76a18d720f680647b622dab150
Lorenzo Guerrini - lorenzo.guerrini5@studio.unibo.it <lorenzo.guerrini5@studio.unibo.it>
2022-12-12 12:14:55 +0100

    Tag section fix

--- template/explore-content.php
+++ template/explore-content.php
@@ -1,34 +1,24 @@
-<section class="filter-section">
-    <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10">
-            <div class="row tag-h2-container">
-                <h2>Tags</h2>
-            </div>
-            <div class="row search-bar-container">
-                <input type="text" placeholder="Search a tag"/>
-            </div>
-            <section class="tag-list-container">
-                <?php foreach($templateParams["tags"] as $tag): ?>
-                        <div class="row">
-                            <!--div class="col-1">
-                                <input id="input-<?php echo $tag["name"]; ?>" type="checkbox" onclick="handleClick(this)"/>
-                            </div>
-                            <div class="col-11">
-                                <label for="input-<?php echo $tag["name"]; ?>"><?php echo $tag["name"]; ?></label>
-                            </div-->
-                            <div class="col-12">
-                                <label>
-                                    <input type="checkbox" onclick="handleClick(this)"/>
-                                    <?php echo $tag["name"]; ?>
-                                </label>
-                            </div>
-                        </div>
-                <?php endforeach; ?>
-            </section>
+<section class="row filter-section">
+    <div class="col-1"></div>
+    <div class="col-10">
+        <div class="row tag-h2-container">
+            <h2>Tags</h2>
         </div>
-        <div class="col-1"></div>
+        <div class="row search-bar-container">
+            <input type="text" placeholder="Search a tag"/>
+        </div>
+        <section class="row tag-list-container">
+            <?php foreach($templateParams["tags"] as $tag): ?>
+                <div class="col-12">
+                    <label>
+                        <input type="checkbox" onclick="handleClick(this)"/>
+                        <?php echo $tag["name"]; ?>
+                    </label>
+                </div>
+            <?php endforeach; ?>
+        </section>
     </div>
+    <div class="col-1"></div>
 </section>
 <div class="row">
     <div class="col-12 posts-container">
