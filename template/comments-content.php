1d852964518f4916d7a45e274f239f56a7ce68e2
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2022-12-27 15:23:29 +0100

    Make write a comment section style

--- template/comments-content.php
+++ template/comments-content.php
@@ -1,11 +1,11 @@
 <?php if($templateParams["postId"] != -1): ?>
     <section id="post-info-section">
         <article id="recipe-article" class="row">
-            <div class="col-4 p-0 d-flex align-items-center">
+            <div class="col-3 p-0 d-flex align-items-center">
                 <img src="<?php echo $templateParams["postDetails"][0]["preview"]; ?>" alt="Recipe image"/>
             </div>
-            <div class="col-8 row ms-2 align-items-center">
-                <p><?php echo $templateParams["postDetails"][0]["owner"]; ?></p>
+            <div class="col-9 row ms-2 align-items-center">
+                <p id="owner-username"><?php echo $templateParams["postDetails"][0]["owner"]; ?></p>
                 <p><?php echo $templateParams["postDetails"][0]["description"]; ?></p>
             </div>
         </article>
@@ -14,12 +14,14 @@
         <?php foreach($templateParams["comments"] as $comment): ?>
             <div class="single-comment-container">
                 <article class="row comment-article">
-                    <div class="col-4 p-0 propic-container">
+                    <div class="col-3 p-0 propic-container">
                         <img src="<?php echo $PROFILE_PIC_DIR.$comment["profilePic"];; ?>" alt="Recipe image" />
                     </div>
-                    <div class="col-8 row ms-2 align-items-center">
-                        <p class="col-6"><?php echo $comment["user"] ?></p>
-                        <p class="col-6"><?php echo $comment["date"] ?></p>
+                    <div class="col-9 row p-0 ms-2 align-items-center">
+                        <div class="col-12 row pt-2">
+                            <p class="col-6"><?php echo $comment["user"] ?></p>
+                            <p class="col-6"><?php echo $comment["date"] ?></p>
+                        </div>
                         <p class="col-12"><?php echo $comment["content"] ?></p>
                     </div>
                 </article>
@@ -28,12 +30,14 @@
     </section>
     <section id="write-comment-section" class="row">
         <form class="col-12 row" action="" method="POST">
-            <div class="col-10">
+            <div id="comment-input-container" class="col-11">
                 <label for="comment-input">
                     <input id="comment-input" type="text" name="comment-text" placeholder="Write a comment..." />
                 </label>
             </div>
-            <input class="col-2" type="submit" value="" />
+            <div class="col-1 p-0 d-flex align-items-center">
+                <input id="post-comment-input" class="col-2" type="submit" value="" />
+            </div>
         </form>
     </section>
 <?php endif; if($templateParams["postId"] == -1): ?>
