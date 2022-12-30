26be490b161b7084f65f27228314744f9bd39d73
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2022-12-30 14:41:38 +0100

    Redirect if no page is found

---
+++ not-found.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+$templateParams["title"] = "OnlyFood - 404";
+$templateParams["name"] = "not-found-content.php";
+$templateParams["style"] = array("base-style.css");
+
+require 'template/base.php';
+?>
--- profile.php
+++ profile.php
@@ -5,8 +5,11 @@ $templateParams["title"] = "OnlyFood - Profile";
 $templateParams["name"] = "profile-content.php";
 
 if (isset($_GET["user"])) {
+    if (!$dbh->userAlreadyRegistered($_GET["user"])) {
+        header("Location: ./not-found.php");
+    }
 
-    if (isset($_SESSION["username"]) && $_SESSION["username"] == $_GET["user"]) {
+    if (isset($_SESSION["username"]) && strtolower($_SESSION["username"]) == strtolower($_GET["user"])) {
         header("Location: ./profile.php");
     }
 
@@ -14,16 +17,9 @@ if (isset($_GET["user"])) {
     $templateParams["profile"] = $dbh->getProfileInfo($_GET["user"]);
     $templateParams["favouriteIngredients"] = $dbh->getMostUsedIngredients($_GET["user"]);
 } else {
-    /* TODO: remove, for debug only */
-    if (isset($_SESSION["username"])) {
-        $templateParams["posts"] = $dbh->getUserPosts($_SESSION["username"]);
-        $templateParams["profile"] = $dbh->getProfileInfo($_SESSION["username"]);
-        $templateParams["favouriteIngredients"] = $dbh->getMostUsedIngredients($_SESSION["username"]);    
-    } else {
-        $templateParams["posts"] = $dbh->getUserPosts($_SESSION["username"]);
-        $templateParams["profile"] = $dbh->getProfileInfo($_SESSION["username"]);
-        $templateParams["favouriteIngredients"] = $dbh->getMostUsedIngredients($_SESSION["username"]);    
-    }
+    $templateParams["posts"] = $dbh->getUserPosts($_SESSION["username"]);
+    $templateParams["profile"] = $dbh->getProfileInfo($_SESSION["username"]);
+    $templateParams["favouriteIngredients"] = $dbh->getMostUsedIngredients($_SESSION["username"]);
 }
 
 $templateParams["style"] = array("profile-style.css", "posts-style.css");
