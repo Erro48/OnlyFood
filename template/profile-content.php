fdeddd50bd956b0bc4df4d26e0b08def26ba9bbd
Lorenzo Guerrini <lorenzo.guerrini5@studio.unibo.it>
2022-12-31 11:14:20 +0100

    Profile section refactor

--- template/profile-content.php
+++ template/profile-content.php
@@ -1,19 +1,19 @@
 <?php $profile = $templateParams["profile"]; ?>
 <div class="row">
-    <div class="col-md-2"></div>
-    <section class="profile-section m-0 col col-md-8 pt-3 pb-3 profile-position"> <!-- profile container -->
+    <div class="col-md-1"></div>
+    <section class="profile-section m-0 col col-md-10 pt-3 pb-3 profile-position"> <!-- profile container -->
         <div class="row mb-4"> <!-- propic-name-followers -->
             <div class="col-1"></div>
-            <div class="col-3">
+            <div class="col-3 col-md-2">
                 <img class="profile-pic" src="<?php echo $PROFILE_PIC_DIR.$profile["profilePic"]; ?>" alt="Propic of <?php echo $profile["username"]; ?>"/>
             </div>
             <div class="col">
                 <div class="row">
-                    <div class="col-8 mini-box">
+                    <div class="col-10 col-md-8 mini-box">
                         <p class="username-text m-0"><?php echo $profile["username"]; ?></p>
                     </div>
                     <?php if (!isset($_GET["user"])): ?>
-                    <div class="col-2 mini-box">
+                    <div class="col-2 mini-box d-flex justify-content-end">
                         <button class="p-2 icon-button hideable" onclick="logout()">
                             <img src="imgs/icons/logout.svg" alt="Logout" />
                         </button>
@@ -23,30 +23,32 @@
                         <div class="p-4">
                         </div>
                     </div>
-                    <?php endif ?>   
+                    <?php endif ?>
+                    <div class="col-md-2"></div>
                 </div>
                 <div class="row">
                     <div class="col mini-box">
                         <p class="mb-3 name-text"><?php echo $profile["name"]." ".$profile["surname"]; ?></p>
                     </div>
-                    <div class="col-1 mini-box">
-                    </div>
                 </div>
                 <div class="row">
-                    <div class="col-4 mini-box">
+                    <div class="col-6 col-md-4 mini-box">
                         <div class="follow-container justify-content-center hideable">                    
                             <p class="m-0">Following</p>
                             <p class="m-0"><?php echo $profile["numFollowing"] ?></p>
                         </div>
                     </div>
-                    <div class="col-4 mini-box">
+                    <div class="col-md-2 d-none d-md-block"></div>
+                    <div class="col-6 col-md-4 mini-box">
                         <div class="follow-container justify-content-center hideable">                    
                             <p class="m-0">Followers</p>
                             <p class="m-0"><?php echo $profile["numFollower"] ?></p>
                         </div>
                     </div>
+                    <div class="col-md-2"></div>
                 </div>
             </div>
+            <div class="col-1"></div>
         </div>
         <div class="row mt-4 justify-content-center">
             <div class="col-11">
@@ -74,7 +76,7 @@
             </div>
         </div>
     </section>
-    <div class="col-md-2"></div>
+    <div class="col-md-1"></div>
 </div>
 <section class="row posts-section"> <!-- posts container -->
     <h2 class="mt-1">Posts</h2>
