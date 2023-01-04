7c3c560c2eaa474b2db88ab5a069b2ae878dcece
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2023-01-04 15:25:11 +0100

    Merge create-post into develop

--- script/posts-script.js
+++ script/posts-script.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-addLoadEventOnload(setPostsHeight);
-addLoadEventOnresize(setPostsHeight);
-=======
-window.onload = (event) => {
-	setPostsHeight()
-}
-
-window.onresize = (event) => {
-	setPostsHeight()
-}
->>>>>>> create-post
+addLoadEventOnload(setPostsHeight)
+addLoadEventOnresize(setPostsHeight)
 
 function setPostsHeight() {
 	const posts = document.querySelector('div.posts-container').children
@@ -19,30 +9,18 @@ function setPostsHeight() {
 }
 
 function calculateRecipeSectionHeight(article) {
-<<<<<<< HEAD
-    const img = new Image();
-    img.src = article.querySelector("img").src;
-    const ratio = img.width / img.height;
-    const height = article.offsetWidth / ratio;
-    if(height > 350){
-        article.querySelector("section.recipe-section").style.height = "".concat(height, "px");
-    } else {
-        article.querySelector("section.recipe-section").style.height = "350px";
-    }
-=======
 	const img = new Image()
 	img.src = article.querySelector('img').src
 	const ratio = img.width / img.height
 	const height = article.offsetWidth / ratio
-	if (height > 300) {
+	if (height > 350) {
 		article.querySelector('section.recipe-section').style.height = ''.concat(
 			height,
 			'px'
 		)
 	} else {
-		article.querySelector('section.recipe-section').style.height = '300px'
+		article.querySelector('section.recipe-section').style.height = '350px'
 	}
->>>>>>> create-post
 }
 
 function showPicture(id) {
@@ -62,18 +40,6 @@ function showPicture(id) {
 	img.style.display = 'inline-block'
 }
 
-<<<<<<< HEAD
-function showRecipe(id){
-    const div = document.querySelector("article#article-".concat(id, " > section.recipe-section"));
-    const img = document.querySelector("article#article-".concat(id, " > img"));
-    const pictureInput = document.querySelector("article#article-".concat(id, " > footer > ul > li:first-child > input"));
-    const recipeInput = document.querySelector("article#article-".concat(id, " > footer > ul > li:nth-child(2) > input"));
-    pictureInput.classList.remove("preview-selected-left");
-    recipeInput.classList.add("preview-selected-right");
-    calculateRecipeSectionHeight(document.querySelector("article#article-".concat(id)));
-    img.style.display = "none";
-    div.style.display = "flex";
-=======
 function showRecipe(id) {
 	const div = document.querySelector(
 		'article#article-'.concat(id, ' > section.recipe-section')
@@ -87,9 +53,11 @@ function showRecipe(id) {
 	)
 	pictureInput.classList.remove('preview-selected-left')
 	recipeInput.classList.add('preview-selected-right')
+	calculateRecipeSectionHeight(
+		document.querySelector('article#article-'.concat(id))
+	)
 	img.style.display = 'none'
 	div.style.display = 'flex'
->>>>>>> create-post
 }
 
 function like(id) {
