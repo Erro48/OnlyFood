21bd9675fdfafa4599008948a7937314cda7db81
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2023-01-05 17:32:02 +0100

    Separate modal stuff into js and css modules

--- post.php
+++ post.php
@@ -3,8 +3,8 @@ require_once 'bootstrap.php';
 
 $templateParams["title"] = "OnlyFood - Create Post";
 $templateParams["name"] = "create-post.php";
-$templateParams["style"] = array("create-post-style.css");
-$templateParams["script"] = array("cookie.js", "create-post-script.js", "search-ingredient.js", "search-tag.js");
+$templateParams["style"] = array("create-post-style.css", "modal.css");
+$templateParams["script"] = array("cookie.js", "modal.js", "create-post-script.js", "search-ingredient.js", "search-tag.js");
 
 require 'template/base.php';
 ?>
