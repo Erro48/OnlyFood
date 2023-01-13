92272178871a6fafb0df7797b6085e4c98f54750
Marco Sternini <mkarko01@gmail.com>
2023-01-13 16:26:43 +0100

    Add recipes search
    - Fix Posts.php ingredients outline style

--- template/search-content.php
+++ template/search-content.php
@@ -28,53 +28,57 @@
             <div class="row">
                 <div class="col-1 col-md-3"></div>
                 <div class="col">
-                    <input class="ps-3 pe-2" type="search" onkeyup="search(this)" placeholder="Search recipe here..." />
+                    <input class="ps-3 pe-2" type="search" onkeyup="searchData(this)" placeholder="Search recipe here..." />
                 </div>
                 <div class="col-1 col-md-3"></div>
             </div>
-        </section>
-
-        <div id="simple-ingredients-list">
-        </div>
-
-        <input type="button" value="Add ingredient" class="button-secondary w-100" data-bs-toggle="modal" data-bs-target="#add-ingredient-modal" onclick="loadModal(ModalsType.SIMPLE_INGREDIENTS, simpleIngredientsCallback)">
-
-        <section class="modal fade" id="add-ingredient-modal" tabindex="-1" aria-labelledby="add-ingredient-modal-label" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <header class="modal-header">
-                        <h4 class="modal-title" id="add-ingredient-modal-label">Choose the ingredients</h4>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </header>
-
-                    <section class="modal-body">
-                        <div class="row">
-                            <label for="search-simple-ingredients" class="h-auto">
-                                <input 
-                                    type="search"
-                                    name="search-simple-ingredient"
-                                    id="search-simple-ingredients"
-                                    class="search-modal"
-                                    onkeyup="searchModal(this)"
-                                    aria-label="Search an ingredient"
-                                    placeholder="Search an ingredient...">
-                                <span class="d-none">Search an ingredient</span>
-                            </label>
-                        </div>
-
-                        <!-- Dropdown menu -->
-                        <div class="search-result-container p-0 col-12">
-                            <ul class="search-result p-0" id="search-simple-ingredients-result">
-                            </ul>
-                        </div>
-                        <div class="modal-list row w-100 m-auto mt-4 scrollable" id="modal-simple-ingredients-list">
-                        </div>
-                    </section>
-                    <footer class="modal-footer">
-                        <button type="button" class="button-primary w-100" data-bs-dismiss="modal" onclick="addSimpleIngredients()">Add ingredient</button>
-                    </footer>
-                </div>
+            
+            <!--
+            <div id="simple-ingredients-list">
             </div>
+
+            <button class="button-primary w-100" data-bs-toggle="modal" data-bs-target="#add-ingredient-modal" onclick="loadModal(ModalsType.SIMPLE_INGREDIENTS, simpleIngredientsCallback)">
+                Add ingredient
+            </button>
+            -->
+            
+            <section class="modal fade" id="add-ingredient-modal" tabindex="-1" aria-labelledby="add-ingredient-modal-label" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <header class="modal-header">
+                            <h4 class="modal-title" id="add-ingredient-modal-label">Choose the ingredients</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </header>
+
+                        <section class="modal-body">
+                            <div class="row">
+                                <label for="search-simple-ingredients" class="h-auto">
+                                    <input 
+                                        type="search"
+                                        name="search-simple-ingredient"
+                                        id="search-simple-ingredients"
+                                        class="search-modal"
+                                        onkeyup="searchModal(this)"
+                                        aria-label="Search an ingredient"
+                                        placeholder="Search an ingredient...">
+                                    <span class="d-none">Search an ingredient</span>
+                                </label>
+                            </div>
+
+                            <!-- Dropdown menu -->
+                            <div class="search-result-container p-0 col-12">
+                                <ul class="search-result p-0" id="search-simple-ingredients-result">
+                                </ul>
+                            </div>
+                            <div class="modal-list row w-100 m-auto mt-4 scrollable" id="modal-simple-ingredients-list">
+                            </div>
+                        </section>
+                        <footer class="modal-footer">
+                            <button type="button" class="button-primary w-100" data-bs-dismiss="modal" onclick="addSimpleIngredients()">Add ingredient</button>
+                        </footer>
+                    </div>
+                </div>
+            </section>
         </section>
 
         <!-- Users search -->
@@ -82,7 +86,7 @@
             <div class="row">
                 <div class="col-1 col-md-3"></div>
                 <div class="col">
-                    <input class="ps-3 pe-2" type="search" onkeyup="search(this)" placeholder="Search here..." />
+                    <input class="ps-3 pe-2" type="search" onkeyup="searchData(this)" placeholder="Search here..." />
                 </div>
                 <div class="col-1 col-md-3"></div>
             </div>
